659350c098d6645494f32139e153fcf4bbd2e005
Marcos Mendez <marcos.antonio.mendez2002@gmail.com>
2025-10-25 23:44:42 -0300

    feat: lógica inicial en Repository y Service de Paquete [Marcos]

---
+++ src/main/java/ar/edu/unju/fi/Repository/PaqueteRepository.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.Repository;
+
+import ar.edu.unju.fi.model.Paquete;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PaqueteRepository extends JpaRepository<Paquete, Long> {
+    List<Paquete> findByPesoKgBetween(Double p, Double p2);
+    List<Paquete> findByVolumenDm3Between(Double v, Double v2);
+}
---
+++ src/main/java/ar/edu/unju/fi/Service/PaqueteService.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.Service;
+
+import ar.edu.unju.fi.Repository.PaqueteRepository;
+import ar.edu.unju.fi.model.Paquete;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaqueteService {
+    private PaqueteRepository paqueteRepository;
+
+    public PaqueteService(PaqueteRepository paqueteRepository) {
+        this.paqueteRepository = paqueteRepository;
+    }
+
+    public List<Paquete> ListarPorPeso(Double p1, Double p2){
+        return paqueteRepository.findByPesoKgBetween(p1, p2);
+    }
+
+    public List<Paquete> ListarPorVolumen(Double v1, Double v2){
+        return paqueteRepository.findByVolumenDm3Between(v1, v2);
+    }
+}
