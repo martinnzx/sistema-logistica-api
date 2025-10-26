ccc4228bc3c6e4416621bb5b00e4e44ced3d731d
Marcos Mendez <marcos.antonio.mendez2002@gmail.com>
2025-10-25 23:34:11 -0300

    feat: Agregar Repository y Service de Envio [Marcos]

---
+++ src/main/java/ar/edu/unju/fi/Repository/EnvioRepository.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.Repository;
+
+
+import ar.edu.unju.fi.model.Envio;
+import ar.edu.unju.fi.model.EstadoEnvio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface EnvioRepository extends JpaRepository<Envio,Long> {
+    List<Envio> findByRemitenteIgnoreCase(String remitente);
+
+    List<Envio> findByDestinatarioIgnoreCase(String destinatario);
+
+    List<Envio> findByEstado(EstadoEnvio estado);
+}
